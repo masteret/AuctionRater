a5a432939c14a6ca8b005a7d36f4875a3c972beb
masteret <kaheichan@utexas.edu>
2015-09-03 16:03:55 -0500

    reconstructed rater.py to clearer

--- rater.py
+++ rater.py
@@ -4,57 +4,54 @@ from PIL import Image, ImageTk
 import os
 import shutil
 
-path = ""
-files = []
-count = 0
+rating = {}
 
-def rate(i):
-    if len(files) <= count:
-        print "All images are processed"
+def rate(root, i):
+    if len(files) > 0:
+        print files[0]
+        rating[files[0]] = i
+
+        img = ImageTk.PhotoImage(Image.open(files[0]))
+        root.title(files[0])
+
+        root.viewer = Label(root, image=img)
+        root.viewer.image = img
+        root.viewer.grid(row=0, column=0, columnspan=5)
+        files.remove(files[0])
+    else:
         root.destroy()
-
-    count += 1
-
-    img = ImageTk.PhotoImage(Image.open(path + str(files[count])))
-    root.title(path + str(files[count]))
-
-    root.viewer = Label(root, image=img)
-    root.viewer.image = img
-    root.viewer.grid(row=0, column=0, columnspan=5)
     return True
 
 def build_windows():
     root = Tk()
 
+    img = ImageTk.PhotoImage(Image.open(files[0]))
     viewer = Label(root, image=img)
-    viewer.image = img
     viewer.grid(row=0, column=0, columnspan=5)
 
     rate1 = Button(root, text="1", width="16", height="5")
     rate1.grid(row=1, column=0)
-    rate1["command"] = lambda: rate(1)
+    rate1["command"] = lambda: rate(root, 1)
 
     rate2 = Button(root, text="2", width="16", height="5")
     rate2.grid(row=1, column=1)
-    rate2["command"] = lambda: rate(2)
+    rate2["command"] = lambda: rate(root, 2)
 
     rate3 = Button(root, text="3", width="16", height="5")
     rate3.grid(row=1, column=2)
-    rate3["command"] = lambda: rate(3)
+    rate3["command"] = lambda: rate(root, 3)
 
     rate4 = Button(root, text="4", width="16", height="5")
     rate4.grid(row=1, column=3)
-    rate4["command"] = lambda: rate(4)
+    rate4["command"] = lambda: rate(root, 4)
 
     rate5 = Button(root, text="5", width="16", height="5")
     rate5.grid(row=1, column=4)
-    rate5["command"] = lambda: rate(5)
+    rate5["command"] = lambda: rate(root, 5)
 
     root.mainloop()
 
 if __name__ == "__main__":
-    print "please input the image folder path"
-    path = raw_input()
-    path = os.path.abspath(path)
-    files = [x for x in os.listdir(path)]
+    path = os.path.abspath(sys.argv[1])
+    files = [os.path.join(path, x) for x in os.listdir(path)]
     build_windows()
